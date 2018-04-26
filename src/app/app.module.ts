d258b161af40868d57d10304010503fb287c2cff
gregg95 <grzegorzgiera95@gmail.com>
2018-04-26 22:14:16 +0200

    Dodanie logowania z google plus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,42 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { GooglePlus } from '@ionic-native/google-plus';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCpJ_KAdkIpjqwcE6SNcea4jCfiZ1juzWM",
+  authDomain: "justcontrol-ab259.firebaseapp.com",
+  databaseURL: "https://justcontrol-ab259.firebaseio.com",
+  projectId: "justcontrol-ab259",
+  storageBucket: "justcontrol-ab259.appspot.com",
+  messagingSenderId: "426681299685"
+}
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage
   ],
   providers: [
+    GooglePlus,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
---
+++ src/pages/login/login.ts
@@ -0,0 +1,88 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import { ToastController } from 'ionic-angular';
+import { GooglePlus } from '@ionic-native/google-plus';
+import { Platform } from 'ionic-angular';
+import firebase from 'firebase';
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  user: Observable<firebase.User>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private afAuth: AngularFireAuth,
+    private gplus: GooglePlus, private toastCtrl: ToastController,
+    private platform: Platform) {
+      
+      this.afAuth.authState.subscribe(res => {
+        this.makeToast(res.displayName);
+      });
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+
+
+
+  googleLogin() {
+    if (this.platform.is('cordova')){
+      this.nativeGoogleLogin();
+    } else {
+      this.webGoogleLogin();
+    }
+  }
+
+  async nativeGoogleLogin(): Promise<void> {
+    try {
+      const gplusUser = await this.gplus.login({
+        'webClientId': '426681299685-15viaq14deolshshhnj80gpfrqhvli4f.apps.googleusercontent.com',
+        'offline': true,
+        'scopes': 'profile email'      
+      });
+
+      return await this.afAuth.auth.signInWithCredential(
+        firebase.auth.GoogleAuthProvider.credential(gplusUser.idToken)
+      ).then(r => {
+        this.makeToast("zalogowano " + JSON.stringify(r));
+      })
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  async webGoogleLogin(): Promise<void> {
+    try {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      const credential = await this.afAuth.auth.signInWithPopup(provider).then(r => {
+        this.makeToast("zalogowano " + JSON.stringify(r));
+      });
+      
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  signOut() {
+    this.afAuth.auth.signOut();
+    if (this.platform.is('cordova')) {
+      this.gplus.logout();
+    }
+  }
+
+  makeToast(message) {
+    this.toastCtrl.create({
+      message: message,
+      duration: 3000,
+      position: 'top'
+    }).present();    
+  }
+
+}
