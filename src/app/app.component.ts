f1218f9f118df5a035a352f8c6c3b21c855b4118
188216 <anetasosnowska@hotmail.com>
2018-04-28 13:09:50 +0200

    dodanie funkcji do przyciskow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
-import { Platform, ToastController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, ToastController, NavController, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { LoginPage } from '../pages/login/login';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { GooglePlus } from '@ionic-native/google-plus';
+import { MainPage } from '../pages/main/main';
+import { TasksHistoryPage } from '../pages/tasks-history/tasks-history';
+import { CommonExpensesPage } from '../pages/common-expenses/common-expenses';
+import { RankingPage } from '../pages/ranking/ranking';
+import { SettingsPage } from '../pages/settings/settings';
+import { AboutPage } from '../pages/about/about';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) navCtrl;
   rootPage:any = LoginPage;
   
   constructor(private platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
@@ -26,6 +33,34 @@ export class MyApp {
 
 
   }
+
+  goToMain(){
+    this.navCtrl.popTo(MainPage);
+  }
+  
+  goToTasksHistory(){
+    this.navCtrl.push(TasksHistoryPage);
+  }
+  
+  goToCommonExpanses(){
+    this.navCtrl.push(CommonExpensesPage);
+  }
+  
+  goToRanking(){
+    this.navCtrl.push(RankingPage);
+  }
+  
+  goToSettings(){
+    this.navCtrl.push(SettingsPage);
+  }
+  
+  goToAbout(){
+    this.navCtrl.push(AboutPage);
+  }
+
+
+
+
 
   logout(){
 
